74bb738bdac15acb9c9b0bc52ace566e61a1d87a
lxlGhostlylxl <xcyberechox@gmail.com>
2022-03-14 00:18:03 -0500

    updated

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -3,35 +3,31 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    tags: ["tag1", "tag2", "tag3"],
+    tags: ["tag1", "tag2"],
   };
+
+  handleIncrement = () =>{
+    console.log("Success");
+    this.state.count += 1;
+  }
+
+  renderTags(){
+    if(this.state.tags.length === 0) return <p>Theres no tags!</p>;
+
+    return <ul>{this.state.tags.map(tag => <li key={tag}> {tag} </li>)} </ul>;
+  }
 
   render() {
     return (
       <div>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm" fun>
-          Increment
-        </button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        <span className="badge bg-warning -m2">{this.state.count}</span>
+      <button onClick={this.handleIncrement} className ='btn btn-secondary btn-sm'>+</button>
+      {this.renderTags()}
       </div>
     );
   }
 
-  getBadgeClasses() {
-    let classes = "badge m-2 bg-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
 
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
-  }
 }
 
 export default Counter;
