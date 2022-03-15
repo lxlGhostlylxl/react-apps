90cbfecec4e927fa8cbef8f68811420fbf151e9e
lxlghostlylxl <xcyberechox@gmail.com>
2022-03-15 12:30:51 -0500

    init

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
+    value: this.props.counter.value,
   };
 
   handleIncrement = () => {
@@ -13,35 +13,38 @@ class Counter extends Component {
     if (this.state.value > 0) this.setState({ count: (this.state.value -= 1) });
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>Theres no tags!</p>;
-
-    return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}> {tag} </li>
-        ))}
-      </ul>
-    );
+  formatIndex() {
+    const { count } = this.state.value;
+    return count === 0 ? "Zero" : count;
   }
 
   render() {
     let classes = "badge m-2 ";
     classes += this.state.value === 0 ? "bg-warning" : "bg-primary";
+
     return (
       <div>
-        <span className={classes}>{this.state.value}</span>
+        <span className={classes}>{this.formatIndex()}</span>
+
         <button
           onClick={this.handleIncrement}
           className="btn btn-secondary btn-sm"
         >
           +
         </button>
+
         <button
           onClick={this.handleDeincrement}
           className="btn btn-danger btn-sm"
         >
           -
+        </button>
+
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
