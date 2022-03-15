efd031fb82fdbe6cf371d6511bd7239776a47ff6
lxlGhostlylxl <xcyberechox@gmail.com>
2022-03-15 18:23:08 -0500

    updated

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -1,40 +1,23 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value,
-  };
-
-  handleIncrement = () => {
-    this.setState({ value: (this.state.value += 1) });
-  };
-
-  handleDeincrement = () => {
-    if (this.state.value > 0) this.setState({ count: (this.state.value -= 1) });
-  };
-
-  formatIndex() {
-    const { count } = this.props.counter.value;
-    return count === 0 ? "Zero" : count;
-  }
-
   render() {
     let classes = "badge m-2 ";
-    classes += this.state.value === 0 ? "bg-warning" : "bg-primary";
+    classes += this.props.counter.value === 0 ? "bg-warning" : "bg-primary";
 
     return (
       <div>
         <span className={classes}>{this.formatIndex()}</span>
 
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           +
         </button>
 
         <button
-          onClick={this.handleDeincrement}
+          onClick={() => this.props.onDecrement(this.props.counter)}
           className="btn btn-danger btn-sm"
         >
           -
@@ -48,6 +31,10 @@ class Counter extends Component {
         </button>
       </div>
     );
+  }
+  formatIndex() {
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 }
 
